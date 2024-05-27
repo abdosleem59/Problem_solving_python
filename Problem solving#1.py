5fb9f790e3ebd780a8c169128be6e1c375829c7f
abdosleem59 <44243387+abdosleem59@users.noreply.github.com>
2024-05-27 06:51:01 -0700

    Add files via upload

---
+++ Problem solving#1.py
@@ -0,0 +1,53 @@
+# Problem: Pair of numbers
+# Description: Read 3 integers N, M, SUM
+# Find total number of pairs that satisfy 
+# A + B == SUM where 
+# 1 <= A <= N  and 1 <= B <= 
+
+# Brute force solution:
+'''
+n,m,sum = map(int, input().split())
+cnt =0
+
+for i in range(1, n+1):
+    for j in range(1, m+1):
+        if i+j == sum:
+            cnt+=1
+
+print(cnt)
+'''
+###############################################
+
+# Creative solution that take much less time and space
+n, m, sum = map(int,input().split())
+
+res = None
+# Case#1 
+if sum>n+m:
+    res = "There are no pairs"
+# Case#2
+elif sum==n+m:
+    res = 1
+
+else:
+    # Case#3 Like: 70 70 70 OR Like: 200 300 70
+    if n >= sum and m >= sum:
+        res = sum-1
+    
+    # Case#4 Like: 50 60 70 OR 60 50 70
+    elif n < sum and m < sum:
+        if n>=m:
+            res = m - (sum - n) + 1
+        else:
+            res = n - (sum - m) + 1
+
+    # Case#5 Like 40 80 70 OR 80 40 70 OR 50 70 70
+    elif n < sum or m < sum:
+        if n>=sum:
+            res = m
+        elif m>=sum:
+            res = n
+
+print(res)
+
+    
